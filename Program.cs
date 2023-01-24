745ebe4befb44de86e98553bd22bb90344a6de17
Roman Guydmn <romanguydmn@gmail.com>
2023-01-24 06:02:06 +0400

    Задача 13 Семинар 2_решил

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int y = 10;
+int x1 = 99;
+int count = 0;
+int N = a;
+int ind1 = 0;
+int ind2 = 0;
+
+if (a > x1)
+{
+        while (N > 0)
+        {
+            count = count + 1;
+            N = N / 10;
+            ind1 = Convert.ToInt32(Math.Pow(y,count-2));
+            ind2 = Convert.ToInt32(Math.Pow(y,count-3));
+        }  
+        Console.WriteLine($"каккое количество цифр в чсиле {count}");
+        Console.WriteLine($"индекс {ind1}");
+        Console.WriteLine($"индекс {ind2}");
+        
+    Console.WriteLine($"Третья цифра числа {(a%ind1/ind2)}");
+}    
+
+
+
